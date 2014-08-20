6d602c6eeb02d186c4ec693ab72e2fb3dee6c4f1
Riqwan Thamir <riqwan@germ.io>
2014-08-20 12:53:31 +0530

    User Changes
    Made the code cleaner and better.

--- blog.rb
+++ blog.rb
@@ -12,9 +12,20 @@ require 'securerandom'
 class Blog < Sinatra::Base
 
 	helpers do
-		def get_user_object(username)
-			user = User.find_by(username: username)
+		def get_user_object(column, value)
+			user = User.where(column => value).first
 			return user
+		end
+
+		def create_session_token(user)
+			session_token = SecureRandom.uuid
+			user.update_attributes(session_token: session_token)
+			return session_token
+		end
+
+		def get_session_token(column, value)
+			user = User.where(column => value).first
+			return user.session_token
 		end
 	end
 
@@ -23,28 +34,34 @@ class Blog < Sinatra::Base
   	enable :sessions
   	register Sinatra::Flash
 	
-	before %r{^(?!(/|/login|/signup|/confirm/([\w]+)/([\w]+))$)} do
+	before %r{^(?!(/|/login|/signup|/confirm/([\w]+)|/space)$)} do
         redirect '/' if !check_login?
     end
 
-    get "/confirm/:user/:token" do
-		user = User.find_by(username: params[:user])
-		if user.token == params[:token]
-			user.update_attributes(activated: true)
-			flash[:post] = "Successfully activated. Login!"
-			session[:username] = user.username
-			redirect '/'
+	get "/" do 
+		if check_login?
+			@posts = Post.order("created_at DESC") 
+			@author = session[:username]
+			erb :post_index
+		else
+			erb :home_page, :layout => false
 		end
 	end
-	
-	get "/" do 
-  		if check_login?
-  			@posts = Post.order("created_at DESC") 
-  			@author = session[:username]
-  			erb :post_index
-  		else
-  			erb :home_page, :layout => false
-  		end
+
+ 	# --------------------------- START USER ----------------------------- #
+
+    get "/confirm/:token" do
+		if user = get_user_object(:activation_token, params[:token])
+			flash[:login] = "You're already Activated"
+			redirect '/login' if user.activated
+			user.update_attributes(activated: true)
+			flash[:post] = "Successfully activated. Login!"
+			session[:id] = create_session_token(user)
+			redirect '/'
+		else
+			flash[:login] = "Invalid Token"
+			redirect '/login'
+		end
 	end
 
 	get '/login' do
@@ -58,6 +75,13 @@ class Blog < Sinatra::Base
     	log_user_in
   	end
 
+
+	get "/logout" do
+	    session[:id] = nil
+	    flash[:home] = "Successfully logged out!"
+	    redirect "/"
+  	end
+
 	get "/signup" do
 		if check_login?
 			redirect '/'
@@ -69,12 +93,14 @@ class Blog < Sinatra::Base
 		create_user(params)
 	end
 
+ 	# --------------------------- END USER ----------------------------- #
+
+ 	# --------------------------- START POSTS ----------------------------- #
+
 	get '/posts/new' do
 		@post = Post.new
 		erb :post_new
 	end
-
-	
 
 	post "/posts" do
 		params[:post][:author] = session[:username]
@@ -114,6 +140,10 @@ class Blog < Sinatra::Base
 		redirect '/'
 	end
 
+ 	# --------------------------- END POSTS ----------------------------- #
+
+ 	# --------------------------- START COMMENTS ----------------------------- #
+
 	post "/posts/:id/comment" do
 		user = User.find_by(username: session[:username])
 		post = Post.find(params[:id])
@@ -136,10 +166,7 @@ class Blog < Sinatra::Base
 		redirect "/posts/#{@post_id}"
 	end
 
-	get "/logout" do
-	    session[:username] = nil
-	    flash[:home] = "Successfully logged out!"
-	    redirect "/"
-  	end
+ 	# --------------------------- END COMMENTS ----------------------------- #
+
 
 end
